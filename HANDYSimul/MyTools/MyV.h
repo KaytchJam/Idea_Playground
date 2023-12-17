8d132d8ca875b1c4310c46f9190b06689d1ebb32
KJam <kaytchjam@gmail.com>
2023-12-16 23:14:47 -0600

    matrix scalar operations added

--- HANDYSimul/MyTools/MyV.h
+++ HANDYSimul/MyTools/MyV.h
@@ -28,6 +28,10 @@ namespace lalg {
     float dot(const vec4& v1, const vec4& v2) { return v1.r * v2.r + v1.g * v2.g + v1.b * v2.b + v1.a * v2.a; }
     void printVec(const vec4& v) { std::printf("[%f, %f, %f, %f]\n", v.r, v.g, v.b, v.a); }
 
+    // special vectors
+    vec4 const onesVec() { return { 1, 1, 1, 1 }; }
+    vec4 const zeroVec() { return { 0, 0, 0, 0 }; }
+
     //
     // MATRICES
     //
@@ -78,8 +82,32 @@ namespace lalg {
                 {0, 0, 0, 1}};
     }
 
+    // returns a 4 * 4 matrix of ones
+    mat4 const onesMat() {
+        return {{1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1}};
+    }
+
+    // returns a 4 * 4 matrix of zeroes
+    mat4 const zeroesMat() {
+        return {{0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}};
+    }
+
+    // creates a matrix with values of vector v along the diagonal
+    mat4 const diag(const vec4& v) {
+        return {{v.r, 0, 0, 0},
+                {0, v.g, 0, 0},
+                {0, 0, v.b, 0},
+                {0, 0, 0, v.a}};
+    }
+
     // rotation with respect to the x (or red) axis
-    mat4 const rotationMatX(float theta) {
+    mat4 const rotationMatX(const float theta) {
         return { {1,           0,                0, 0},
                 {0, cosf(theta), -1 * sinf(theta), 0},
                 {0, sinf(theta),      cosf(theta), 0},
@@ -87,7 +115,7 @@ namespace lalg {
     }
 
     // rotation with respect to the y (or green) axis
-    mat4 const rotationMatY(float theta) {
+    mat4 const rotationMatY(const float theta) {
         return { {     cosf(theta), 0, sinf(theta), 0},
                 {               0, 1,           0, 0},
                 {-1 * sinf(theta), 0, cosf(theta), 0},
@@ -95,21 +123,52 @@ namespace lalg {
     }
 
     // rotation with respect to the z (or blue) axis
-    mat4 const rotationMatZ(float theta) {
+    mat4 const rotationMatZ(const float theta) {
         return {{cosf(theta), -1 * sinf(theta), 0, 0},
                 {sinf(theta),      cosf(theta), 0, 0},
                 {          0,                0, 1, 0},
                 {          0,                0, 0, 1} };
     }
 
-    // matrix vector and matrix matrix operations
+    // matrix vector operations
     vec4 operator*(const mat4& m, const vec4& v) { return { dot(m.r1, v), dot(m.r2, v), dot(m.r3, v), dot(m.r4, v) }; }
+    // matrix scalar operations
+    mat4 operator*(const float f, const mat4& m) { return { f * m.r1, f * m.r2, f * m.r3, f * m.r4 }; }
+    mat4 operator*(const mat4& m, const float f) { return f * m; }
+    mat4 operator/(const mat4& m, const float f) { return { m.r1 / f, m.r2 / f, m.r3 / f, m.r4 / f }; }
+    mat4 operator/(const float f, const mat4 & m) { return f * m; }
+    // matrix matrix operations
     mat4 operator+(const mat4& m1, const mat4& m2) { return { m1.r1 + m2.r1, m1.r2 + m2.r2, m1.r3 + m2.r3, m1.r4 + m2.r4 }; }
     mat4 operator*(const mat4& m1, const mat4& m2) {
         mat4 col_major = { m1 * getColumn(m2, 0), m1 * getColumn(m2, 1), m1 * getColumn(m2, 2), m1 * getColumn(m2, 3) };
         return transpose(col_major);
     }
+    
+    // apply some function func to all elements in the matrix and return a new matrix
+    mat4 map(const mat4& m, float (*func)(float)) {
+        mat4 out = zeroesMat();
+        float* target_ptr = (float*) & out;
+        float* from_ptr = (float*)&m;
 
+        for (int i = 0; i < 16; i++) {
+            *target_ptr = func(*from_ptr);
+            target_ptr++;
+            from_ptr++;
+        }
+
+        return out;
+    }
+
+    // apply some function func to all elements in the matrix, and update the matrix
+    void mapInPlace(mat4* m, float(*func)(float)) {
+        float* elem_ptr = (float*)&m;
+        for (int i = 0; i < 16; i++) {
+            *elem_ptr = func(*elem_ptr);
+            elem_ptr++;
+        }
+    }
+
+    // print da matrix
     void printMat(const mat4& m) {
         printVec(m.r1);
         printVec(m.r2);

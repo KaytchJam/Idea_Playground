127a0c201ea1a724004cb0c3c88e6070c338da84
KJam <kaytchjam@gmail.com>
2023-12-27 23:25:02 -0600

    Addition setColor function made that takes in a uint32

--- HANDYSimul/CustomShapes/ColumnShape.h
+++ HANDYSimul/CustomShapes/ColumnShape.h
@@ -6,7 +6,7 @@ private:
 	sf::CircleShape base, top;
 	sf::RectangleShape bar;
 public:
-	ColumnShape(float radius, float height) {
+	ColumnShape(float radius = 0, float height = 0) {
 		top = sf::CircleShape(radius);
 		top.setPosition({0, 0});
 		top.setFillColor(sf::Color(0x808080FF));
@@ -51,10 +51,25 @@ public:
 		top.setFillColor(sf::Color(r / 3, g / 3, b / 3, 0xFF));
 	}
 
+	void setColor(uint32_t color) {
+		uint8_t colors[3] = {0x0, 0x0, 0x0};
+		const uint32_t s = color;
+
+		color = color >> 8; // get rid of alpha channel
+		for (int i = 0; i < 3; i++) {
+			colors[2 - i] = color & 0xFF;
+			color = color >> 8;
+		}
+
+		printf("Color: 0x%08X, R: 0x%02X G: 0x%02X B: 0x%02X\n", s, colors[0], colors[1], colors[2]);
+		setColor(colors[0], colors[1], colors[2]);
+	}
+
 	// getters
 	float getHeight() const { return bar.getSize().y; }
 	float getRadius() const { return base.getRadius();  }
 	sf::Vector2f getPosition() const { return top.getPosition(); }
+	sf::Color getColor() const { return base.getFillColor(); }
 
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
 		target.draw(base, states);
